4240acb72a6541a693c5ce90ad290fddcccc3f35
ivanmac2019 <55422326+ivanmac2019@users.noreply.github.com>
2022-11-04 08:31:36 -0500

    se agrega el proyecto

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using system;
+using system.Net;
+
+{
+    string API_URL = "https://bpoamericas.co/Prueba/SecurityAPI/Security/LoginUser";
+   
+
+    var client = new WebClient();
+    client.downloadstring(API_URL); 
+}
+
